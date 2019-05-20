c53a4e90f0aa84ad1948e3a628b3b796bb6743da
whhub <wanghui122004@me.com>
2019-05-20 20:05:32 +0800

    [00012] spring [007] sprint data source

--- spring/datasource/src/main/java/uih/isd/spring/hello/datasource/DatasourceApplication.java
+++ spring/datasource/src/main/java/uih/isd/spring/hello/datasource/DatasourceApplication.java
@@ -1,13 +1,44 @@
 package uih.isd.spring.hello.datasource;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class DatasourceApplication {
+@Slf4j
+public class DatasourceApplication implements CommandLineRunner {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatasourceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+		showData();
+	}
+
+	private void showConnection() throws SQLException {
+		System.out.println(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		System.out.println(conn.toString());
+		conn.close();
+	}
+
+	private void showData(){
+		jdbcTemplate.queryForList("SELECT * FROM FOO").forEach(row->System.out.println(row.toString()));
+	}
 }
