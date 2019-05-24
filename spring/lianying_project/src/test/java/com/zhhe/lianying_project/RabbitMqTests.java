5a5d4bad24b7f97c76bf0493e250da3b7b3fb4dd
whhub <wanghui122004@me.com>
2019-05-24 15:58:13 +0800

    [00023] spring [015] [FastJson][00] start spring.io

--- spring/lianying_project/src/test/java/com/zhhe/lianying_project/RabbitMqTests.java
+++ spring/lianying_project/src/test/java/com/zhhe/lianying_project/RabbitMqTests.java
@@ -1,14 +1,6 @@
 package com.zhhe.lianying_project;
 
 import com.zhhe.lianying_project.rabbitmq.Producer;
-import javafx.application.Application;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import com.zhhe.lianying_project.rabbitmq.Producer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
